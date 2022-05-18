977106842bc96d9415f1ab8d0cc679ec14ccc13d
risingBirdSong <heregoingthere@outlook.com>
2022-05-17 20:44:25 -0700

    clean

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
@@ -15,7 +14,6 @@
     flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
       pkgs = import nixpkgs {
         inherit system;
-        config.allowBroken = true;
       };
       ee-try-flakes = pkgs.callPackage ./ee-try-flakes.nix {};
     in {
@@ -23,9 +21,5 @@
         inherit pkgs;
       };
       defaultPackage = ee-try-flakes;
-      # packages = flake-utils.lib.flattenTree {
-      #   inherit cataract;
-      #   inherit docker;
-      # };
     });
 }
